d995e623bac6b73b1befb96db84bfcb13e36e478
leanne <motieleannefotso@gmail.com>
2020-11-26 11:29:58 +0100

    modification

---
+++ CC01.BLL/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC01.BLL
+{
+    public class StudentBLL
+    {
+        StudentBLL studentRepo;
+        private String dbFolder;
+        public StudentBLL (string dbFolder) 
+        {
+            studentRepo = new StudentBLL(dbFolder);
+        }
+        public void CreateStudent (StudentBLL student)
+        {
+            studentRepo.Add(student);
+        }
+
+        private void Add(StudentBLL student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteStudent(StudentBLL student)
+        {
+            studentRepo.Remove(student);
+        }
+
+        private void Remove(StudentBLL student)
+        {
+            throw new NotImplementedException();
+        }
+        public IEnumerable<Student> GetAllStudents()
+        {
+            return studentRepo.Find();
+        }
+        public IEnumerable<Student>
+    }
+
+    internal class Student
+    {
+    }
+}
---
+++ CC01.BO/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC01.BO
+{
+    public class Student
+    {
+        public string UserName { get; set; }
+        public string Matricule { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Lieu_Naissance { get; set; }
+        public string Date_Naissance { get; set; }
+public Student (string userName, string matricule, string fullName, string email, string lieu_Naissance, string date_Naissance)
+        {
+            UserName = userName;
+            Matricule = matricule;
+            FullName = fullName;
+            Email = email;
+            Lieu_Naissance = lieu_Naissance;
+            Date_Naissance = date_Naissance;
+        }
+    }
+}
